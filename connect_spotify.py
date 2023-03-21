505399c0320f975d25653d92b5a7f37c526f7e0e
Maia <mayalapi@gmail.com>
2023-03-21 21:59:12 +0200

    changed code architecture

--- connect_spotify.py
+++ connect_spotify.py
@@ -9,32 +9,39 @@ load_dotenv()
 client_id = os.getenv("CLIENT_ID")
 client_secret  = os.getenv("CLIENT_SECRET")
 
-def get_token():
-    #Encode the client id and secret to access spotify api
-    auth_string = client_id + ":" + client_secret
-    auth_bytes = auth_string.encode("utf-8")
-    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
+#class Token: --how to do it when one function depends on the other 
+class Token: 
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        
+    def get_token(self):
+        #Encode the client id and secret to access spotify api
+        auth_string = client_id + ":" + client_secret
+        auth_bytes = auth_string.encode("utf-8")
+        auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
-    #Add headers to the token post request and other shit for the json request
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": "Basic " + auth_base64,
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-    data = { "grant_type": "client_credentials" }
+        #Add headers to the token post request and other shit for the json request
+        url = "https://accounts.spotify.com/api/token"
+        headers = {
+            "Authorization": "Basic " + auth_base64,
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+        data = { "grant_type": "client_credentials" }
 
-    # send a post request to spotify server
-    result = post(url, headers=headers, data=data)
+        # send a post request to spotify server
+        result = post(url, headers=headers, data=data)
 
-    #returning the response of the post request in a json format
-    json_result = json.loads(result.content)
+        #returning the response of the post request in a json format
+        json_result = json.loads(result.content)
 
-    #accessing the value of the access token key to just get the access token and return it
-    token = json_result["access_token"]
-    return token
+        #accessing the value of the access token key to just get the access token and return it
+        token = json_result["access_token"]
+        return token
 
-#format the token
-def get_auth_header(token):
-    return {"Authorization": "Bearer " + token}
-
-token = get_token()
+    #format the token
+    def get_auth_header(self,token):
+        try:
+            return {"Authorization": "Bearer " + token}
+        except:
+            print("no token provided")
--- spotify.py
+++ spotify.py
@@ -1,36 +1,51 @@
-from connect_spotify import token, get_auth_header
+from connect_spotify import Token
+from connect_spotify import client_id, client_secret
 from dotenv import load_dotenv
 import os
 import base64
 import json
 from requests import post, get
 
-# find the list of aritsts when you search for one 
-def find_artists(token,artist):
-    url = 'https://api.spotify.com/v1/search?q={}&type=artist&limit=1'.format(artist)
-    headers = get_auth_header(token)
+# initialize and get the token string from the class Token at connect_spotify
+token = Token(client_id, client_secret)
+my_token = token.get_token()
 
+# send request to spotify api
+def send_request(url):
+    headers = token.get_auth_header(my_token)
     result = get(url, headers=headers) 
     json_result = json.loads(result.content)
     return json_result
+
+# find the list of aritsts when you search for one 
+def find_artists(artist):
+    url = 'https://api.spotify.com/v1/search?q={}&type=artist&limit=1'.format(artist)
+    response = send_request(url)
+    return response
+
 
 # get the top tracks of the artist that you searched
-def get_top_tracks(token):
-    artist = 'metallica'
-    artist_id = find_artists(token, artist)['artists']['items'][0]['id']
-
+def get_top_tracks():
+    artist = 'Odaya'
+    artist_id = find_artists(artist)['artists']['items'][0]['id']
     # send a get request
     url = 'https://api.spotify.com/v1/artists/{}/top-tracks?country=IL'.format(artist_id)
-    headers = get_auth_header(token)
-    result = get(url, headers=headers) 
-    json_result = json.loads(result.content)
-    return json_result
+    response = send_request(url)
+    return response
 
-# format the json file and take out the necessary data
+# format the json file and take out the necessary data, write it into songs.txt
 def make_tracks_list(json):
     tracks_list = json['tracks']
+    # delete previous content of file songs.txt, soon to be filled with the top tracks
+    delete_file_content  = open("songs.txt",'w')
     for index, song in enumerate(tracks_list):
         print(f"{index + 1}. {song['name']}")
+        with open("songs.txt", "a") as f:
+            print(f"{index + 1}. {song['name']}", file=f)
 
 
-ready_tracks = make_tracks_list(get_top_tracks(token))
+def main():
+    ready_tracks = make_tracks_list(get_top_tracks())
+
+if __name__ == "__main__":
+    main()
